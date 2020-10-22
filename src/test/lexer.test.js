5302a1325f74785413f34e40ad032c2fd28080b6
Sam Carlile <samkcarlile@gmail.com>
2020-10-21 20:25:21 -0500

    more tests and some very small refactoring

--- src/test/lexer.test.js
+++ src/test/lexer.test.js
@@ -21,21 +21,24 @@ const expectTypes = (tokens, expectedTypes) =>
 
 ////////////////////////////////////////////////////
 
-test('it properly lexes hello_world.umu', () => {
+test('lexes hello_world.umu', () => {
   const contents = getData('hello_world.umu');
   const tokens = withoutWhitespace(tokenize(contents));
 
   expect(tokens).toHaveLength(5);
   expectTypes(tokens, [
+    // 👏 📟
     'import',
     'identifier',
+
+    // 📟🗣 "Hello World!"
     'identifier',
     'identifier',
     'string_literal',
   ]);
 });
 
-test('it properly lexes user_input.umu', () => {
+test('lexes user_input.umu', () => {
   const contents = getData('user_input.umu');
   const tokens = withoutWhitespace(tokenize(contents));
 
@@ -62,5 +65,141 @@ test('it properly lexes user_input.umu', () => {
     'interp_end',
     'const',
     'template_end',
+  ]);
+});
+
+test('if statement', () => {
+  const input = `🤔 👍 🌴 👋 🌴`;
+  const tokens = withoutWhitespace(tokenize(input));
+
+  expect(tokens).toHaveLength(5);
+
+  expectTypes(tokens, [
+    'if',
+    'true',
+    'block_delimiter',
+    'exit',
+    'block_delimiter',
+  ]);
+});
+
+test('function chaining', () => {
+  const input = `📟🗣 ⬅️ 📟👂 "What is your name?"`;
+  const tokens = withoutWhitespace(tokenize(input));
+
+  expect(tokens).toHaveLength(6);
+
+  expectTypes(tokens, [
+    'identifier',
+    'identifier',
+    'chain',
+    'identifier',
+    'identifier',
+    'string_literal',
+  ]);
+});
+
+test('type conversion', () => {
+  const input = `
+  🐶 = 🔡 123456
+  🐱 = 🔢 "99.1"
+  🐐 = ☯️ 1
+  `;
+  const tokens = withoutWhitespace(tokenize(input));
+
+  expect(tokens).toHaveLength(12);
+
+  expectTypes(tokens, [
+    'identifier',
+    'assignment',
+    'toString',
+    'number_literal',
+
+    'identifier',
+    'assignment',
+    'toNumber',
+    'string_literal',
+
+    'identifier',
+    'assignment',
+    'toBoolean',
+    'number_literal',
+  ]);
+});
+
+test('math', () => {
+  const input = `
+  🦁 = 🐶 + 🐱
+  😀 = 2 * 50.0 - 🐶 / 600
+  😞 = 24 % 5 +-45
+  `;
+  const tokens = withoutWhitespace(tokenize(input));
+
+  expect(tokens).toHaveLength(22);
+
+  expectTypes(tokens, [
+    // 🦁 = 🐶 + 🐱
+    'identifier',
+    'assignment',
+    'identifier',
+    'plus',
+    'identifier',
+
+    // 😀 = 2 * 50.0 - 🐶 / 600
+    'identifier',
+    'assignment',
+    'number_literal',
+    'multiply',
+    'number_literal',
+    'minus',
+    'identifier',
+    'divide',
+    'number_literal',
+
+    // 😞 = 24 % 5 +-45
+    'identifier',
+    'assignment',
+    'number_literal',
+    'modulo',
+    'number_literal',
+    'plus',
+    'minus',
+    'number_literal',
+  ]);
+});
+
+test('boolean expression', () => {
+  const input = `
+  🎉 = 34 🌖 1
+  🐰 = 🎉 🌓 👍
+  🎃 = 🐰 && 6 🌔 10
+  `;
+  const tokens = withoutWhitespace(tokenize(input));
+
+  expect(tokens).toHaveLength(17);
+
+  expectTypes(tokens, [
+    // 🎉 = 34 🌖 1
+    'identifier',
+    'assignment',
+    'number_literal',
+    'gt',
+    'number_literal',
+
+    // 🐰 = 🎉 🌓 👍
+    'identifier',
+    'assignment',
+    'identifier',
+    'eq',
+    'true',
+
+    // 🎃 = 🐰 && 6 🌔 10
+    'identifier',
+    'assignment',
+    'identifier',
+    'and',
+    'number_literal',
+    'lt',
+    'number_literal',
   ]);
 });
